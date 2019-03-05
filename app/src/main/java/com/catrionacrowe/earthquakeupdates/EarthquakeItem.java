3c32a54be48330dbc9ed7dc034633739912a288d
Catriona Crowe <catrionacrowee@gmail.com>
2019-03-05 14:12:31 +0000

    adding the EarthquakeItem class

---
+++ app/src/main/java/com/catrionacrowe/earthquakeupdates/EarthquakeItem.java
@@ -0,0 +1,59 @@
+package com.catrionacrowe.earthquakeupdates;
+
+public class EarthquakeItem {
+
+    private String title;
+    private String description;
+    private String link;
+    private String pubdate;
+    private String category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubdate() {
+        return pubdate;
+    }
+
+    public void setPubdate(String pubdate) {
+        this.pubdate = pubdate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "title=" + title + '\n' +
+                ", description=" + description + '\n' +
+                ", link='" + link + '\n' +
+                ", pubdate=" + pubdate + '\n' +
+                ", category=" + category + '\n';
+    }
+}
--- app/src/main/java/com/catrionacrowe/earthquakeupdates/MainActivity.java
+++ app/src/main/java/com/catrionacrowe/earthquakeupdates/MainActivity.java
@@ -1,10 +1,13 @@
+/**
+ * Created by Catriona Crowe
+ */
+
 package com.catrionacrowe.earthquakeupdates;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
