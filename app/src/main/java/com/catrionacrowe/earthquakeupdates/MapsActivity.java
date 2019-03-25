cb00dee2e384767022dc6edbabeb89c82a2d5ba6
Catriona Crowe <catrionacrowee@gmail.com>
2019-03-25 21:30:00 +0000

    enhanced the onMapReady function so that it loops round all earthquake items and drops a marker with description.split to get the location

--- app/src/main/java/com/catrionacrowe/earthquakeupdates/MapsActivity.java
+++ app/src/main/java/com/catrionacrowe/earthquakeupdates/MapsActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -32,15 +34,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        ArrayList<EarthquakeItem> earthquakes = EarthquakeParser.getEarthquakes();
+
         //set the centre location for zoom purposes
         LatLng centreLocation = new LatLng(54.072701, -4.653230);
-        // Add a marker using lat and long coordinates and move the camera to the location plotted
-        LatLng latLng = new LatLng(51.163, -0.241);
-        mMap.addMarker(new MarkerOptions().position(latLng).title("NEWDIGATE, SURREY"));
+
+        for (EarthquakeItem eq : earthquakes) {
+
+            String [] titlesplit = eq.getTitle().split(":",-1);
+
+            //get lat
+            double varlat = Double.parseDouble(eq.getGeoLat());
+
+            //get long
+            double varlong =  Double.parseDouble(eq.getGeoLong());
+
+            //set position of earthquake
+            LatLng position = new LatLng(varlat, varlong);
+
+            // Add a marker using lat and long coordinates and move the camera to the location plotted
+            mMap.addMarker(new MarkerOptions().position(position).title(titlesplit[2]));
+
+            }
+
+        //zoom to centre on GB by 5
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(centreLocation, 5), 5000, null);
+        //change map type to hybrid
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
     }
 }
